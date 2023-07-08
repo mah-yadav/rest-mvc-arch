b7833f0191493b9c8380683d14de67aa8fca52f8
mah-yadav <mahe.2008.yadav@gmail.com>
2023-07-08 14:32:19 +0530

    Initial Project Setup

---
+++ restservice/src/test/java/com/mvc/restservice/RestserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mvc.restservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RestserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
